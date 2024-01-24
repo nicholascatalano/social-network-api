ff1be2c3cbaa0c8ab64ed6a8d5eb9e78a1bfacf0
nickcatalano5 <nickcatalano24@gmail.com>
2024-01-24 17:04:11 -0500

    initializes User model and exports

--- models/User.js
+++ models/User.js
@@ -43,3 +43,8 @@ const userSchema = new Schema(
 userSchema.virtual("friendCount").get(function () {
   return this.friends.length;
 });
+
+// Initialize User model using Schema
+const User = model("User", userSchema);
+
+module.exports = User;
