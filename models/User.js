141ced0ae5637f8f1724f04f26688a7c2f4e901d
nickcatalano5 <nickcatalano24@gmail.com>
2024-01-24 17:01:53 -0500

    adds User Schema

---
+++ models/User.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require("mongoose");
+
+// Schema to create User model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Enter a valid email address.",
+      ], // regex to check for valid email
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
