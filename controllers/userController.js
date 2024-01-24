60e6b908648a7eb99d4993146d582c2fb9519722
nickcatalano5 <nickcatalano24@gmail.com>
2024-01-24 17:45:52 -0500

    adds more outline to userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,11 +2,13 @@ const { User, Thought } = require("../models");
 
 const userController = {
   // GET all users
-  
+  async getUsers(req, res) {},
   // GET a single user by its _id and populated thought and friend data
   // POST a new user
   // PUT to update a user by its _id
   // DELETE to remove user by its _id
+  // add a friend
+  // remove a friend
 };
 
 module.exports = userController;
