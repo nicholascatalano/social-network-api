e4acce0076d1f600e641641073bb7a70ee5a0639
nickcatalano5 <nickcatalano24@gmail.com>
2024-01-24 17:21:58 -0500

    adds virtual to get the length of the thoughts reactions array field on query

--- models/Thought.js
+++ models/Thought.js
@@ -27,3 +27,8 @@ const thoughtSchema = new Schema(
     id: false,
   }
 );
+
+// Creates a virtual called reactionCount that retrieves the length of the thought's reactions array field on query.
+userSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
