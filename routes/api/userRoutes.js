cac635896199bf851debafc1a9b9aea0b960bb3e
nickcatalano5 <nickcatalano24@gmail.com>
2024-01-25 17:42:45 -0500

    adds user route to delete friend from user friends list

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -9,6 +9,7 @@ const {
   updateUser,
   deleteUser,
   addFriend,
+  deleteFriend,
 } = require("../../controllers/userController");
 
 // Route -> http://localhost:3001/api/users
@@ -18,7 +19,7 @@ router.route("/").get(getUsers).post(createUser);
 router.route("/:userId").get(getUser).put(updateUser).delete(deleteUser);
 
 // Route -> http://localhost:3001/api/users/:userId/friends/:friendId
-router.route("/:userId/friends/:friendId").post(addFriend);
+router.route("/:userId/friends/:friendId").post(addFriend).delete(deleteFriend);
 
 // Exports
 module.exports = router;
